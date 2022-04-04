5194d4185d8f6d468ed11a3ba5d6da3e786a0251
xujianlei <xujianlei@163.com>
2022-04-04 21:12:22 +0800

    命名路由

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/','StaticPagesController@home');
-Route::get('/help','StaticPagesController@help');
+Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about');
