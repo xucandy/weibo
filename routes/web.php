56b93fff104862df0c5ca2e90edccd66428542e6
xujianlei <xujianlei@163.com>
2022-04-04 22:01:51 +0800

    dddd#

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/','StaticPagesController@home');
 Route::get('/help','StaticPagesController@help')->name('help');
-Route::get('/about','StaticPagesController@about');
+Route::get('/about','StaticPagesController@about')->name('about');
+Route::get('/signup','UserController@create')->name('signup');
