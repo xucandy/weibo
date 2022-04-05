a5a87a72df7110866e98e3fa4e400377743f9768
xujianlei <xujianlei@163.com>
2022-04-05 15:00:11 +0800

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title','Webibo App')</title>
-    <link rel="stylesheet" href="{{mix('/css/app.css')}}">
+    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
+
   <body>
     @include('layouts._header')
+
     <div class="container">
-    @yield('content')
-     @include('layouts._foot')
+      <div class="offset-md-1 col-md-10">
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._foot')
+      </div>
     </div>
   </body>
 </html>
